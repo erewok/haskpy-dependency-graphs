8e29b935cb500760ce174bae33df34f0a3f549dc
eraker <eraker@gmail.com>
2015-07-13 22:01:33 -0700

    Initial function to turn a module into a graph.

--- src/DependencyGraph/GraphModules.hs
+++ src/DependencyGraph/GraphModules.hs
@@ -1,5 +1,21 @@
 module DependencyGraph.GraphModules (
-                                    ) where
+  Graph,
+  graphModule
+  ) where
+
+import Control.Monad
+import DependencyGraph.Modules as M
+
+
+data Graph = Graph { head :: FilePath
+                   , nodes :: IO [FilePath]}
+
+
+
+graphModule :: M.Environment -> FilePath -> Graph
+graphModule env infile = do
+  let paths = M.findAllModules env infile
+  Graph infile paths
 
 -- Walk directory: http://rosettacode.org/wiki/Walk_a_directory/Recursively#Haskell
 -- check file exists: http://rosettacode.org/wiki/Check_that_file_exists#Haskell

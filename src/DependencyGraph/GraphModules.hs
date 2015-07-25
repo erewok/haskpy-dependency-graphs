9d5e775471520cb5bd0f9b32a927cfe8008de0c0
eraker <eraker@gmail.com>
2015-07-24 17:26:04 -1000

    Discovered/Visited nodes logic in place for graph generation.

--- src/DependencyGraph/GraphModules.hs
+++ src/DependencyGraph/GraphModules.hs
@@ -2,7 +2,7 @@
 
 module DependencyGraph.GraphModules (
   Node(..),
-  makeVertices,
+  makeEdges,
   makeNode
   ) where
 
@@ -14,26 +14,62 @@ import qualified DependencyGraph.Modules as M
 
 data Node = Node { node :: FilePath
                  , nodes :: [FilePath]
-                 , vertices :: [(FilePath, FilePath)]}
+                 , edges :: [(FilePath, FilePath)]}
 
-makeVertices :: Functor f => FilePath -> f FilePath -> f (FilePath, FilePath)
-makeVertices infile fps = (,) infile <$> fps
+makeEdges :: Functor f => FilePath -> f FilePath -> f (FilePath, FilePath)
+makeEdges infile fps = (,) infile <$> fps
 
 makeNode :: M.Environment -> IO FilePath -> IO Node
 makeNode env infile = do
   file <- infile
   absfile <- M.absolutize file
   paths <- M.findAllModules env file
-  let nodeVertices = makeVertices absfile paths
-  return $ Node absfile paths nodeVertices
+  let nodeEdges = makeEdges absfile paths
+  return $ Node absfile paths nodeEdges
 
--- processModule :: Node -> [Node] -> [Node]
--- processModule = []
+-- Simple strategy:
+-- "Discovered" nodes are added to [Node] as Node discoveredPath [] []
+-- Visited nodes are then marked Node discoveredPath ["$" ...] [...]
+-- where ... may be an empty list or a list of FilePaths
 
--- graphModules :: M.Environment -> FilePath -> [Node] -> [Node]
--- graphModules env infile [] = []
+nodeDiscovered :: [Node] -> FilePath -> Bool
+nodeDiscovered nodeset fp = fp `elem` (map node nodeset)
 
+undiscoveredNodes :: [Node] -> [FilePath]
+undiscoveredNodes nds = filter (nodeDiscovered nds) $ concatMap nodes nds
 
+makeDiscoveredNode :: FilePath -> Node
+makeDiscoveredNode fp = Node fp [] []
 
--- -- Walk directory: http://rosettacode.org/wiki/Walk_a_directory/Recursively#Haskell
--- -- See: Chapter 18 of Real World Haskell
+discoverAllNodes :: [Node] -> [Node]
+discoverAllNodes allnodes = (++) allnodes $ map makeDiscoveredNode $ undiscoveredNodes allnodes
+
+visited :: [FilePath] -> Bool
+visited [] = False
+visited ("$":_) = True
+visited _ = False
+
+nodeVisited :: Node -> Bool
+nodeVisited = visited . nodes
+
+markVisited :: Node -> Node
+markVisited nd
+  | nodeVisited nd = nd
+  | otherwise = Node infile visitedpaths nodeedges
+                 where infile = node nd
+                       visitedpaths = "$" : nodes nd
+                       nodeedges = edges nd
+
+unvisitedNodes :: [Node] -> [Node]
+unvisitedNodes = filter nodeVisited
+
+visitAllNodes :: M.Environment -> [IO Node] -> IO [Node]
+visitAllNodes env nds = do
+  let allnodes = sequence nds
+  mustvisit <- unvisitedNodes <$> allnodes
+  let tovisit = pure <$> node <$> mustvisit
+  let visited = liftM markVisited . makeNode env <$> tovisit
+  sequence $ (++) nds $ visited
+
+allNodesVisited :: [Node] -> Bool
+allNodesVisited = and . map nodeVisited
